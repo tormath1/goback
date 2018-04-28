a7ab32d523e2ed771a1bd87076391728fac21198
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-28 21:51:38 +0200

    feat(server): add schedule volume command

--- server/main.go
+++ server/main.go
@@ -11,10 +11,12 @@ import (
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/otiai10/copy"
+	"github.com/robfig/cron"
 	pb "github.com/tormath1/goback/server/proto"
 )
 
 var docker *client.Client
+var chronoTable *cron.Cron
 
 func main() {
 	docker, err := client.NewEnvClient()
@@ -29,31 +31,54 @@ func main() {
 	grpcServer := grpc.NewServer()
 	pb.RegisterManagerServer(grpcServer, &server{docker})
 
+	chronoTable = cron.New()
 	log.Fatal(grpcServer.Serve(listener))
 }
 
 type server struct{ docker *client.Client }
 
 func (s *server) SaveVolume(ctx context.Context, in *pb.SaveVolumeRequest) (*pb.Error, error) {
+	out := &pb.Error{Code: 200}
 
 	src := in.VolumeName
 	dst := in.Destination
 
-	volumes, err := s.docker.VolumeList(ctx, filters.Args{})
+	err := save(src, dst, s.docker)
 	if err != nil {
-		msg := fmt.Sprintf("unable to list volumes: %v", err)
-		log.Print(msg)
-		return &pb.Error{Message: msg}, err
+		log.Printf("unable to save volume: %v", err)
+		out.Code = 500
+		out.Message = err.Error()
+	}
+	return out, err
+}
+
+func (s *server) ScheduleSaving(ctx context.Context, in *pb.ScheduleSavingRequest) (*pb.Error, error) {
+
+	out := &pb.Error{Code: 200}
+
+	job := func() { save(in.Volume.VolumeName, in.Volume.Destination, docker) }
+	err := chronoTable.AddFunc(in.Schedule, job)
+	if err != nil {
+		log.Printf("unable to add entry to chrono table: %v", err)
+		out.Code = 500
+		out.Message = err.Error()
+	}
+	return out, err
+}
+
+func save(src, dst string, cli *client.Client) error {
+
+	volumes, err := cli.VolumeList(context.Background(), filters.Args{})
+	if err != nil {
+		return err
 	}
 
 	for _, volume := range volumes.Volumes {
 		if volume.Name == src {
 			if err = copy.Copy(volume.Mountpoint, fmt.Sprintf("%s/_data", dst)); err != nil {
-				msg := fmt.Sprintf("unable to copy volume: %s on %s: %v", volume.Name, dst, err)
-				log.Print(msg)
-				return &pb.Error{Message: msg}, err
+				return err
 			}
 		}
 	}
-	return &pb.Error{}, nil
+	return err
 }
