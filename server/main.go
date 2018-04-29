65c816713f3566d021f813080db824f3172c1bcd
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-29 11:55:46 +0200

    fix(server): remove docker client in save function

--- server/main.go
+++ server/main.go
@@ -34,6 +34,7 @@ func main() {
 	pb.RegisterManagerServer(grpcServer, &server{docker})
 
 	chronoTable = cron.New()
+	chronoTable.Start()
 	entries = make(map[string]string)
 	log.Fatal(grpcServer.Serve(listener))
 }
@@ -54,7 +55,12 @@ func (s *server) SaveVolume(ctx context.Context, in *pb.SaveVolumeRequest) (*pb.
 	src := in.VolumeName
 	dst := in.Destination
 
-	err := save(src, dst, s.docker)
+	mountpoint, err := getMountpoint(src, s.docker)
+	if err != nil {
+		return &pb.Empty{}, err
+	}
+
+	err = save(src, dst, mountpoint)
 	if err != nil {
 		log.Printf("unable to save volume: %v", err)
 	}
@@ -63,11 +69,11 @@ func (s *server) SaveVolume(ctx context.Context, in *pb.SaveVolumeRequest) (*pb.
 
 func (s *server) ScheduleSaving(ctx context.Context, in *pb.ScheduleSavingRequest) (*pb.Empty, error) {
 
-	job := func() { save(in.Volume.VolumeName, in.Volume.Destination, docker) }
-	_, err := getMountpoint(in.Volume.VolumeName, s.docker)
+	mountpoint, err := getMountpoint(in.Volume.VolumeName, s.docker)
 	if err != nil {
 		return &pb.Empty{}, err
 	}
+	job := func() { save(in.Volume.VolumeName, in.Volume.Destination, mountpoint) }
 	err = chronoTable.AddFunc(in.Schedule, job)
 	if err != nil {
 		log.Printf("unable to add entry to chrono table: %v", err)
@@ -76,18 +82,14 @@ func (s *server) ScheduleSaving(ctx context.Context, in *pb.ScheduleSavingReques
 	return &pb.Empty{}, err
 }
 
-func save(src, dst string, cli *client.Client) error {
+func save(src, dst, mountpoint string) error {
 
-	mountpoint, err := getMountpoint(src, cli)
-	if err != nil {
-		return err
-	}
 	timestamp := time.Now().Format(time.RFC3339)
 
-	if err = copy.Copy(mountpoint, fmt.Sprintf("%s/%s-%s/_data", dst, src, timestamp)); err != nil {
+	if err := copy.Copy(mountpoint, fmt.Sprintf("%s/%s-%s/_data", dst, src, timestamp)); err != nil {
 		return err
 	}
-	return err
+	return nil
 }
 
 func getMountpoint(src string, cli *client.Client) (string, error) {
