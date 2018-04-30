c959714d72d54e9e9ed5779b4d6ad8111de21eec
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-30 21:02:25 +0200

    feat(cmd/root): add grpc client

--- cmd/root.go
+++ cmd/root.go
@@ -3,12 +3,25 @@ package cmd
 import (
 	"log"
 
+	pb "github.com/tormath1/goback/server/proto"
+
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "goback ",
 	Short: "schedule your Docker volumes backups with goback",
+}
+
+var manager pb.ManagerClient
+
+func init() {
+	client, err := grpc.Dial("localhost:12800", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("unable to connect to localhost:12800: %v", err)
+	}
+	manager = pb.NewManagerClient(client)
 }
 
 func Execute() {
